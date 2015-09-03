58cc018d05ba6f80dbae8fc7d6de81ad02962a65
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-03 13:03:39 +0200

    Stub tests

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 const test = require('tape-catch');
+
+test.skip('Works with default options', (is) => {});
+
+test.skip('Takes other hashes', (is) => {});
+
+test.skip('Swallows the hash', (is) => {});
+
+test.skip('Uses a different URL base', (is) => {});
+
+test.skip('Fails gracefully', (is) => {});
